072d7c30cd143cd1f9dd37cfe0f04dc5d59a1eb5
Shane24 <shanemoloney3@gmail.com>
2016-03-15 10:58:30 +0000

    Init

---
+++ js/controllers.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/* Controllers */
+myApp.controller('IpsumCtrl', ['$scope', '$http', function($scope, $http) {
+  $scope.quoteCount = 1;
+  $scope.ipsum = '';
+  $scope.getIpsum = function() {
+    $http.get('./ipsum/words.txt').success(function(data) {
+      $scope.ipsum = shuffle(data.split('\n'));
+    });
+  };
+  var shuffle = function(data) {
+    for (var j, x, i = data.length; i; j = parseInt(Math.random() * i, 10), x = data[--i], data[i] = data[j], data[j] = x);
+    return data;
+  };
+  $scope.tooFew = function() {
+    if ($scope.quoteCount < 2) return true;
+    else return false;
+  };
+  $scope.getIpsum();
+  $scope.addIpsum = function() {
+    $scope.quoteCount = $scope.quoteCount + 1;
+  };
+  $scope.lessIpsum = function() {
+    $scope.quoteCount = $scope.quoteCount - 1;
+  };
+}]);
+
+function selectText(containerid) {
+  if (document.selection) {
+    var range = document.body.createTextRange();
+    range.moveToElementText(document.getElementById(containerid));
+    range.select();
+  } else if (window.getSelection) {
+    var range = document.createRange();
+    range.selectNode(document.getElementById(containerid));
+    window.getSelection().addRange(range);
+  }
+}
